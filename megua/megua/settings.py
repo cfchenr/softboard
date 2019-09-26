3b95f6e5755ed0bddb18f703d7fe4fd8194b64e1
Fábio Henriques <fabiomchenriques@ua.pt>
2019-09-26 15:09:38 +0100

    bug correction

--- megua/megua/settings.py
+++ megua/megua/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +142,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
