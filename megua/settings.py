50e1db56a122edc05ee5a2d4b26fa7a1dbb9f6ca
cfchenr <cfchenr@gmail.com>
2019-10-16 23:18:30 +0100

    fix: update allowed hosts on settings.py

--- megua/settings.py
+++ megua/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'eux2t!w1m8pky=txyrx&atj7f$#p480xd1ofey+o5m3bw5ul%y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['megua.herokuapp.com']
+ALLOWED_HOSTS = ['softboardua.herokuapp.com', 'softboarddev.herokuapp.com']
 
 
 # Application definition
